1193d6994530a84506c1cb1c3c01a57d72f89ecf
pablomoga9 <moyagarcia99@gmail.com>
2022-08-07 23:38:30 +0200

    add/ book list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const booksList = document.getElementById('categoryList');
+let actualBook;
 const searchBar = document.getElementById('searchBar');
 const updateSelect = document.getElementById('selectUpdate');
+
 let sendData; 
 searchBar.addEventListener('keyup',(event)=>{
     
@@ -47,20 +49,46 @@ const loadCharacters = async()=>{
     }
 };
 
+async function loadBooks (bookList){
+   console.log(bookList)
+    const res = await fetch('https://api.nytimes.com/svc/books/v3/lists/bookList.json?api-key=RfMnGRAEn2a8ieRUrcEKuqoMckyRLqQf')
+}
+// console.log("ok")
 const displayLists = (list)=>{
-  
+  booksList.innerHTML = "";
     const htmlString = list.map((lists1)=>{
-       
-        return `
+       return  `
         <li class = "book">
-            <h2>${lists1.display_name}</h2>
+            <button onclick="loadBooks(lists1.display_name)">${lists1.display_name}</button>
             <p>${lists1.oldest_published_date}</p><br>
             <p>${lists1.newest_published_date}</p><br>
             <p>${lists1.updated}</p>
         </li>
         `;
+        
     })
     .join('')
     booksList.innerHTML = htmlString;
 }
+
+const displayBooks = (books1)=>{
+    booksList.innerHTML = "";
+    const htmlString = books1.map((book1,i)=>{
+        return `<li class = "book">
+        <h2>${book1.results.books[i].rank}#${book1.results.books[i].title}</h2>
+        <img src="${book1.results.books[i].book_image}">
+        <p>Semanas en lista: ${book1.results.books[i].weeks_on_list}</p>
+        <a href="${book1.results.books[i].buy_links[0].url}">Comprar</a>
+    </li>`
+    })
+    .join('')
+    booksList.innerHTML = htmlString;
+}
+
+
+console.log(actualBook)
 loadCharacters();
+// document.getElementById("listaName").addEventListener('click', event =>{
+//     let listaEvent = event.innerHTML;
+
+// })
