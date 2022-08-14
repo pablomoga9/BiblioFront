f5ba5de04f6cbc4ff35a14c25f8b156b82b017ed
pablomoga9 <moyagarcia99@gmail.com>
2022-08-14 13:50:38 +0200

    update favs

--- scripts/fav.js
+++ scripts/fav.js
@@ -1,24 +1,31 @@
+
+
 let favArray = [];
 
+//Add to fav list a book by its title
 
 async function tryFav(title){
    firebase.auth().onAuthStateChanged(user => {
       if(user){
         
        
-         let favPush;
+        
          var docRef = db.collection('users')
          .where("email","==",user.email)
          .get()
          .then((querySnapshot)=>{
             querySnapshot.forEach((doc)=>{
               let dbData = doc.data().favs;
-             
-              dbData!==""?favArray = dbData:console.log("no register yet")
+            //   console.log()
+            //   dbData.filter(item=>item.title !== title)
+              (dbData!=="" && dbData)?favArray = dbData:console.log("no register yet")
               })
          })
          .then((querySnapshot)=>{
+           
+            
             favArray.push(title)
+           
 
         
          db.collection("users")
@@ -42,13 +49,7 @@ async function tryFav(title){
 }
 
 
-// async function updateFav(arr){
-//    firebase.auth().onAuthStateChanged(user => {
-//       if(user){
-//          let colRef = db.collection("users").doc(user.email)
-//       }
-//    })
-// }
+
 
 
 
--- scripts/favDisplay.js
+++ scripts/favDisplay.js
@@ -1,6 +1,12 @@
 
 let list = document.getElementById("categoryList");
+let navImage = document.getElementById("navImage");
+let userName = document.getElementById("userName");
+let btnLogout = document.getElementById("btnLogout");
 
+navImage.style.display = "block";
+userName.style.display = "block";
+btnLogout.style.display = "block";
 async function displayFavList(){
    
     firebase.auth().onAuthStateChanged(user => {
@@ -14,6 +20,7 @@ async function displayFavList(){
             return `<li class="books">
             <img class="bookImage" src="${element.image}">
             <p>${element.title}</p>
+            <a id="clearOne" onclick="clearOne('${element.title}')">Eliminar de la lista</a>
             </li>`
          })
          .join('')
@@ -24,3 +31,172 @@ async function displayFavList(){
  }
 
  displayFavList()
+
+
+
+ async function clearOne(titleName){
+   let dataArr = [];
+   firebase.auth().onAuthStateChanged(user => {
+      if(user){
+         db.collection("users").where("email","==", user.email).get().then((querySnapshot)=>{ querySnapshot.forEach((doc)=>{
+            dataArr = doc.data().favs;
+
+          })
+          })
+          .then((querySnapshot)=>{
+            const dataFilter = dataArr.filter(item => item.title !== titleName )
+
+            db.collection("users")
+            .doc(user.email)
+            .set({favs:dataFilter},{merge:true})
+            .then(displayFavList())
+            .catch((error)=>{console.log(error)})
+          })
+      }
+   })
+}
+
+async function clearList(){
+   firebase.auth().onAuthStateChanged(user => {
+      if(user){
+         db.collection("users")
+         .doc(user.email)
+         .set({favs:""},{merge:true})
+         .then(list == "")
+         .catch((error)=>{console.log(error)})
+      }
+   })
+}
+
+
+function showProfile(){
+   firebase.auth().onAuthStateChanged(user => {
+   let profileTitle = document.getElementById("profileTitle");
+   let modalProfile = document.getElementById("modal3");
+   let nickName = user.email.split('@')[0];
+   profileTitle.innerHTML = `Perfil: ${nickName}`;
+   modalProfile.showModal();
+   })
+ }
+
+
+ function uploadFile(){
+   firebase.auth().onAuthStateChanged(user => {
+   
+   var storageRef = firebase.storage().ref();
+   var file = document.getElementById("files").files[0];
+   var thisRef = storageRef.child(file.name);
+   console.log(file.name);
+   thisRef
+   .put(file)
+   .then(function (snapshot) {
+    var usersRef = db.collection("users").doc(user.email);
+     var mergeImageDb = usersRef.set({
+       imageName:file.name
+     },{merge:true})
+
+     getUrl();
+     
+   })
+  
+ })
+}
+
+
+function getUrl(){
+ firebase.auth().onAuthStateChanged(user => {
+   if(user){
+     let fileName;
+
+     db.collection("users")
+     .where("email", "==", user.email)
+     .get()
+     .then((querySnapshot) => {
+       querySnapshot.forEach((doc) => {
+         //Sacar nombre del archivo subido a storage
+         fileName = doc.data().imageName;
+        //Subir dicho nombre de archivo haciendo merge a un documento de usuario en firestore
+        var storageRef = firebase.storage().ref(fileName);
+     
+        storageRef
+        .getDownloadURL()
+        .then(function(url){
+          //Subir url a bdd
+          var usersRef = db.collection("users").doc(user.email);
+          console.log(url);
+            var mergeImageDb = usersRef.set({
+              imageUrl:url
+            },{merge:true})
+   
+            getProfileImage();
+          })
+        .catch(function(error){
+          console.log("error encountered")
+        })
+       //  let profileImage = document.getElementById("profileImage");
+       //  let navImage = document.getElementById("navImage");
+       //  profileImage.src = doc.data().imageUrl
+       //  navImage.src = doc.data().imageUrl
+       })
+     })
+   
+   }
+ 
+ 
+})
+}
+
+
+
+function getProfileImage(){
+ firebase.auth().onAuthStateChanged(user => {
+ db.collection("users")
+ .where("email", "==", user.email)
+ .get()
+ .then((querySnapshot) => {
+   querySnapshot.forEach((doc) => {
+     let profileImage = document.getElementById("profileImage");
+     
+     profileImage.src = doc.data().imageUrl
+     navImage.src = doc.data().imageUrl
+     console.log(doc.data().imageUrl)
+   })
+ })
+ })
+}
+
+firebase.auth().onAuthStateChanged(user => {
+   if (user) {
+     console.log(firebase.auth().currentUser);
+     console.log("state = logged")
+     let btnLogin = document.getElementById("btnLogin");
+     let btnSignup = document.getElementById("btnSignup");
+     let btnLogout = document.getElementById("btnLogout");
+     let nickName = user.email.split('@')[0];
+     userName.style.display = "block";
+     userName.innerHTML = nickName;
+     btnLogin.style.display = "none";
+     btnSignup.style.display = "none";
+     btnLogout.style.display = "block";
+     console.log(firebase.auth().currentUser);
+
+
+
+
+     
+   }
+   else {
+     console.log("state = logged out")
+     let btnLogin = document.getElementById("btnLogin");
+     let btnSignup = document.getElementById("btnSignup");
+     let btnLogout = document.getElementById("btnLogout");
+     btnLogin.style.display = "block";
+     btnSignup.style.display = "block";
+     btnLogout.style.display = "none";
+   }
+
+   
+ })
+
+
+getProfileImage();
